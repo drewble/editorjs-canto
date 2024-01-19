8533cce7ecc2f43e41cad1f5deb036c26ef46337
Drew Blake <drew.blake@lonelyplanet.com>
2024-01-19 14:10:25 -0600

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default {
     copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, "src", "index.js"),
-      name: "cantoTool",
+      name: "CantoTool",
       fileName: "canto",
     },
   },
